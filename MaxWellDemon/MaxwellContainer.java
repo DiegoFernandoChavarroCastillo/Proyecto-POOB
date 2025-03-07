f1148a36a06af2d9b65bbe03d90e8cc0a93e62f5
DiegoFernandoChavarroCastillo <diego.chavarro-c@mail.escuelaing.edu.co>
2025-03-06 20:39:04 -0500

    Implementacion de la clase contenedor y particula
    Realice la implementacion de la clase MaxwellContainer y la clase Particula, aun falta hacer algunas modificaciones.

---
+++ MaxWellDemon/MaxwellContainer.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+/**
+ * MaxwellContainer representa el contenedor donde se desarrolla la simulación.
+ * Permite añadir partículas y controlar la visualización.
+ * El contenedor es un rectángulo con una división en la mitad.
+ * 
+ * @author Diego
+ * @version 1.0
+ */
+public class MaxwellContainer {
+    private Rectangle container;
+    private Rectangle division;
+    private List<Particle> particles;
+    private boolean visible;
+
+    /**
+     * Constructor de la clase MaxwellContainer.
+     * @param width Ancho del contenedor.
+     * @param height Alto del contenedor.
+     */
+    public MaxwellContainer(int width, int height) {
+        container = new Rectangle();
+        container.changeSize(height, width);
+        container.moveHorizontal(50);
+        container.moveVertical(50);
+        division = new Rectangle();
+        division.changeSize(height, 5);
+        division.moveHorizontal(50 + width / 2);
+        division.moveVertical(50);
+        division.changeColor("black");
+        particles = new ArrayList<>();
+        visible = false;
+    }
+
+    /**
+     * Hace visible el contenedor con la división y todas sus partículas.
+     */
+    public void makeVisible() {
+        container.makeVisible();
+        division.makeVisible();
+        for (Particle p : particles) {
+            p.makeVisible();
+        }
+        visible = true;
+    }
+
+    /**
+     * Hace invisible el contenedor con la división y todas sus partículas.
+     */
+    public void makeInvisible() {
+        container.makeInvisible();
+        division.makeInvisible();
+        for (Particle p : particles) {
+            p.makeInvisible();
+        }
+        visible = false;
+    }
+
+    /**
+     * Añade una partícula al contenedor.
+     * @param particle La partícula a añadir.
+     */
+    public void addParticle(Particle particle) {
+        particles.add(particle);
+        if (visible) {
+            particle.makeVisible();
+        }
+    }
+
+    /**
+     * Consulta la cantidad de partículas en el contenedor.
+     * @return Número de partículas.
+     */
+    public int getParticleCount() {
+        return particles.size();
+    }
+
+    /**
+     * Termina la simulación limpiando todas las partículas.
+     */
+    public void finish() {
+        makeInvisible();
+        particles.clear();
+    }
+}
---
+++ MaxWellDemon/Particle.java
@@ -0,0 +1,46 @@
+/**
+ * Clase Particle que representa una partícula en la simulación.
+ * 
+ * @author Diego
+ * @version 1.0
+ */
+class Particle extends Circle {
+    private int speedX;
+    private int speedY;
+
+    /**
+     * Constructor para objetos de la clase Particle.
+     * @param xPos Posición X inicial.
+     * @param yPos Posición Y inicial.
+     * @param color Color de la partícula.
+     * @param speedX Velocidad en X.
+     * @param speedY Velocidad en Y.
+     */
+    public Particle(int xPos, int yPos, String color, int speedX, int speedY) {
+        super();
+        changeColor(color);
+        moveHorizontal(xPos);
+        moveVertical(yPos);
+        this.speedX = speedX;
+        this.speedY = speedY;
+        this.makeVisible();
+    }
+
+    /**
+     * Mueve la partícula automáticamente según su velocidad.
+     */
+    public void move() {
+        moveHorizontal(speedX);
+        moveVertical(speedY);
+    }
+
+    /**
+     * Cambia la velocidad de la partícula.
+     * @param newSpeedX Nueva velocidad en X.
+     * @param newSpeedY Nueva velocidad en Y.
+     */
+    public void setSpeed(int newSpeedX, int newSpeedY) {
+        this.speedX = newSpeedX;
+        this.speedY = newSpeedY;
+    }
+}
